e7288f4a18fe95870d74e820d9be4be7e5c1659a
umatbro <umatbroo@gmail.com>
2016-10-19 23:49:08 +0200

    Add files via upload

---
+++ fun.h
@@ -0,0 +1,38 @@
+#include <LiquidCrystal.h>
+
+class Temperature
+{
+	private:
+	int value; //obecna wartość temperatury
+	
+	public:
+	int desired; //pożądana temperatury
+	Temperature(int ,int); //konstruktor
+	int increaseDesired(int howMuch); //zwiększa oczekiwaną (desidred) wartośc o howMuch i zwraca wynik dodawania
+	int decreaseDesired(int howMuch); //zmniejsza oczekiwaną wartość (desired) o howMuch i zwraca wynik odejmowania
+	int getTempValue();
+};
+
+
+Temperature::Temperature(int tv=14, int td=0)
+{
+	value = tv;
+	desired = td;
+}
+
+int Temperature::increaseDesired(int howMuch)
+{
+	if (desired>= 50) return 50;
+	else return desired += howMuch;
+}
+
+int Temperature::decreaseDesired(int howMuch)
+{
+	if (desired<=0) return 0;
+	else return desired -= howMuch;
+}
+
+int Temperature::getTempValue()
+{
+	return value;
+}
---
+++ v0.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+#include "fun.h"
+
+const int LED = 9;
+const int buttonPlus = 11;
+const int buttonMinus = 10;
+const int buttonAccept = 12;
+int temperature = 14;
+int tempDesired = 0;
+Temperature t1(20,40);
+
+LiquidCrystal lcdDisplay(2,3,4,5,6,7);
+
+
+void mrugaj();
+
+
+void setup() {
+  lcdDisplay.begin(16,2);
+  lcdDisplay.setCursor(0,0);
+  lcdDisplay.print("Temperatura: ");
+  lcdDisplay.setCursor(14,0);
+  lcdDisplay.print((String)t1.getTempValue());
+  
+  lcdDisplay.setCursor(0,1);
+  lcdDisplay.print("Ustaw temp.");
+  lcdDisplay.setCursor(15,1);
+  lcdDisplay.print((String)t1.desired +" st.C");
+}
+
+void loop() 
+{
+
+}
