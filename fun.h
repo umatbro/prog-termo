99a7d0a79b919bf75771585fa2dc732217de345c
umatbro <umatbroo@gmail.com>
2016-10-20 18:50:32 +0200

    Add files via upload

--- fun.h
+++ fun.h
@@ -1,38 +1,145 @@
 #include <LiquidCrystal.h>
 
+
+/*
+TEMPERATURA
+*/
+
 class Temperature
 {
 	private:
-	int value; //obecna wartość temperatury
+	float value; //obecna wartość temperatury
 	
 	public:
-	int desired; //pożądana temperatury
-	Temperature(int ,int); //konstruktor
-	int increaseDesired(int howMuch); //zwiększa oczekiwaną (desidred) wartośc o howMuch i zwraca wynik dodawania
-	int decreaseDesired(int howMuch); //zmniejsza oczekiwaną wartość (desired) o howMuch i zwraca wynik odejmowania
-	int getTempValue();
+	float desired; //pożądana temperatury
+	Temperature(float ,float); //konstruktor
+	float increaseDesired(float howMuch); //zwiększa oczekiwaną (desidred) wartośc o howMuch i zwraca wynik dodawania
+	float decreaseDesired(float howMuch); //zmniejsza oczekiwaną wartość (desired) o howMuch i zwraca wynik odejmowania
+	float getTempValue();
+	float tempSimulation(bool heating); //symulacja temperatury
+	bool isHeatingReq();
 };
 
 
-Temperature::Temperature(int tv=14, int td=0)
+Temperature::Temperature(float tv=14, float td=0)
 {
 	value = tv;
 	desired = td;
 }
 
-int Temperature::increaseDesired(int howMuch)
+float Temperature::increaseDesired(float howMuch)
 {
 	if (desired>= 50) return 50;
 	else return desired += howMuch;
 }
 
-int Temperature::decreaseDesired(int howMuch)
+float Temperature::decreaseDesired(float howMuch)
 {
 	if (desired<=0) return 0;
 	else return desired -= howMuch;
 }
 
-int Temperature::getTempValue()
+float Temperature::getTempValue()
 {
 	return value;
 }
+
+
+float Temperature::tempSimulation(bool heating)
+{
+	if (heating == true)
+	{
+		digitalWrite(9,HIGH);
+		delay(500);
+		value += 0.5;
+		return value;
+	}
+	else 
+	{
+		digitalWrite(9,LOW);
+		delay(500);
+		value -= 0.5;
+		return value;
+	}
+}
+
+bool Temperature::isHeatingReq()
+{
+	if(value < desired + 5) return true;
+	else if (value > desired - 3) return false;
+}
+
+
+/*
+PRZYCISK
+*/
+class Button
+{
+	private:
+	int pin;
+	
+	public:
+	Button(int pn);
+	bool isPushed(); //wciśnięcie przycisku; wciśnięty przycisk zwraca wartość true
+};
+
+Button::Button(int pn)
+{
+	pin = pn;
+	pinMode(pin,INPUT_PULLUP);
+}
+
+bool Button::isPushed()
+{
+	if(digitalRead(pin) == HIGH) return true;
+	else return false;
+}
+
+
+/*
+FUNKCJE
+*/
+
+
+//przewijanie napisu po ekranie gdy ilość znaków większa od 16 
+ void lcd16RollString (LiquidCrystal lcd, String napis, int rzad)
+  {
+    if(napis.length()>16){
+    for(int i =0;i<=(napis.length()-16);i++)
+    {
+      //lcd.clear();
+      lcd.setCursor(0,rzad);
+      for(int j=i;j<=i+15;j++)
+      {
+        if(j<napis.length()){lcd.print(napis[j]);}
+      }
+      delay(1000);
+    }}
+    else{lcd.setCursor(0,rzad);
+    for(int i=0;i<napis.length();i++){
+    lcd.print(napis[i]);}
+    }
+  }
+  
+void displayTemp(float value, LiquidCrystal lcd, int row)
+{
+	if (value < 10) //liczby jednocyfrowe
+	{
+		lcd.setCursor(12,row);
+		lcd.print(" ");
+		lcd.setCursor(13,row);
+		lcd.print((String)value);
+	}
+	else if (value >= 10 && value <100 ) //liczby dwucyfrowe
+	{
+		lcd.setCursor(12,row);
+		lcd.print((String)value);
+	}
+	else
+	{
+		lcd.setCursor(13,row);
+		lcd.print("err");
+	}
+}
+
+
--- v0.ino
+++ v0.ino
@@ -1,31 +1,76 @@
 #include <LiquidCrystal.h>
 #include "fun.h"
 
+
 const int LED = 9;
-const int buttonPlus = 11;
-const int buttonMinus = 10;
-const int buttonAccept = 12;
-int temperature = 14;
-int tempDesired = 0;
-Temperature t1(20,40);
+Button buttonPlus(10);
+Button buttonMinus(11);
+Button buttonAccept(12);
+Temperature t1(20,15);
+bool ON = false;
 
 LiquidCrystal lcdDisplay(2,3,4,5,6,7);
 
 
+//funkcje
+void lcd16RollString (LiquidCrystal lcd, String napis, int rzad);
+void displayTemp(float value, LiquidCrystal lcd, int row); //wyświetl wartość value temperatury w wierszu row wyświetlacza lcd
+void buttonPlus_OnClick();
+void buttonMinus_OnClick();
+bool buttonAccept_OnClick();
+
+
 void setup() {
+	//ustawienie pinów
+	pinMode(LED,OUTPUT);
+  digitalWrite(LED,LOW);
+	
+	//ustawienie ekranu
   lcdDisplay.begin(16,2);
   lcdDisplay.setCursor(0,0);
   lcdDisplay.print("Temperatura: ");
-  lcdDisplay.setCursor(14,0);
-  lcdDisplay.print((String)t1.getTempValue());
+  displayTemp(t1.getTempValue(),lcdDisplay,0);
   
   lcdDisplay.setCursor(0,1);
   lcdDisplay.print("Ustaw temp.");
-  lcdDisplay.setCursor(15,1);
-  lcdDisplay.print((String)t1.desired +" st.C");
+  displayTemp(t1.desired,lcdDisplay,1);
 }
 
 void loop() 
 {
+	buttonPlus_OnClick();
+	buttonMinus_OnClick();
+	displayTemp(t1.desired, lcdDisplay, 1);
+	displayTemp(t1.getTempValue(), lcdDisplay, 0);
+	while( buttonAccept_OnClick())
+	{
+		t1.tempSimulation(t1.isHeatingReq());
+		displayTemp(t1.desired, lcdDisplay, 1);
+		displayTemp(t1.getTempValue(), lcdDisplay, 0);
+	}		
+}
 
+
+void buttonPlus_OnClick()
+{
+  if(buttonPlus.isPushed()) 
+  {
+    t1.increaseDesired(0.5);
+    delay(150);
+  }
+}
+
+void buttonMinus_OnClick()
+{
+	if(buttonMinus.isPushed())
+	{
+		t1.decreaseDesired(0.5);
+		delay(150);
+	}
+}
+
+bool buttonAccept_OnClick()
+{
+	if(buttonAccept.isPushed()) return ON = !ON;
+	else return ON;
 }
