da8a7d4848d78804802683187f0956f9aa2f6a7f
umatbro <umatbroo@gmail.com>
2016-10-24 21:08:49 +0100

    Add files via upload

--- fun.h
+++ fun.h
@@ -9,6 +9,7 @@ class Temperature
 {
 	private:
 		float value; //obecna wartość temperatury
+		bool tempGrowth; //!!!tylko do tempSimul
 	
 	public:
 		float 	desired; //pożądana temperatury
@@ -16,58 +17,61 @@ class Temperature
 		float 	increaseDesired(float howMuch); //zwiększa oczekiwaną (desidred) wartośc o howMuch i zwraca wynik dodawania
 		float 	decreaseDesired(float howMuch); //zmniejsza oczekiwaną wartość (desired) o howMuch i zwraca wynik odejmowania
 		float 	getTempValue();
-		float 	tempSimulation(bool heating); //symulacja temperatury
+		float 	tempSimulation(); //symulacja temperatury
 		bool 	isHeatingReq();
 };
 
 
-Temperature::Temperature(float tv=14, float td=0)
-{
-	value = tv;
-	desired = td;
-}
-
-float Temperature::increaseDesired(float howMuch)
-{
-	if (desired>= 50) return 50;
-	else return desired += howMuch;
-}
-
-float Temperature::decreaseDesired(float howMuch)
-{
-	if (desired<=0) return 0;
-	else return desired -= howMuch;
-}
-
-float Temperature::getTempValue()
-{
-	return value;
-}
-
-
-float Temperature::tempSimulation(bool heating)
-{
-	if (heating == true)
+	Temperature::Temperature(float tv=14, float td=0)
 	{
-		digitalWrite(9,HIGH);
-		delay(500);
-		value += 0.5;
+		value = tv;
+		desired = td;
+		tempGrowth = true;
+	}
+
+	float Temperature::increaseDesired(float howMuch)
+	{
+		if (desired>= 50) return 50;
+		else return desired += howMuch;
+	}
+
+	float Temperature::decreaseDesired(float howMuch)
+	{
+		if (desired<=0) return 0;
+		else return desired -= howMuch;
+	}
+
+	float Temperature::getTempValue()
+	{
 		return value;
 	}
-	else 
-	{
-		digitalWrite(9,LOW);
-		delay(500);
-		value -= 0.5;
-		return value;
-	}
-}
 
-bool Temperature::isHeatingReq()
-{
-	if(value < desired + 5) return true;
-	else if (value > desired - 3) return false;
-}
+
+	float Temperature::tempSimulation()
+	{
+		if (tempGrowth == true && value < desired+5)
+		{
+			digitalWrite(11,HIGH);
+			delay(500);
+			value += 0.5;
+			if(value == desired+5) tempGrowth = false;
+			return value;
+		}
+		else 
+		{
+			digitalWrite(11,LOW);
+			delay(500);
+			value -= 0.5;
+			if ( value < desired - 5 ) tempGrowth = true;
+			return value;
+		}
+	}
+
+	bool Temperature::isHeatingReq()
+	{
+		if(value < desired + 5) return true;
+		else if (value > desired - 3) return false;
+	}
 
 
 /*
@@ -76,24 +80,24 @@ PRZYCISK
 class Button
 {
 	private:
-	int pin;
+		int 	pin;
 	
 	public:
-	Button(int pn);
-	bool isPushed(); //wciśnięcie przycisku; wciśnięty przycisk zwraca wartość true
+				Button(int pn);
+		bool 	isPushed(); //wciśnięcie przycisku; wciśnięty przycisk zwraca wartość true
 };
 
-Button::Button(int pn)
-{
-	pin = pn;
-	pinMode(pin,INPUT_PULLUP);
-}
+	Button::Button(int pn)
+	{
+		pin = pn;
+		pinMode(pin,INPUT_PULLUP);
+	}
 
-bool Button::isPushed()
-{
-	if(digitalRead(pin) == LOW) return true;
-	else return false;
-}
+	bool Button::isPushed()
+	{
+		if(digitalRead(pin) == LOW) return true;
+		else return false;
+	}
 
 
 
@@ -105,21 +109,51 @@ bool Button::isPushed()
 class RemoteButton
 {
 	private:
-		int hexCode1;
-		int hexCode2;
+		int 	hexCode1;
+		int 	hexCode2;
+		int 	pin;
 		
 	public:
-		RemoteButton(int);
+	//atrybuty
+		int		buttonUp;
+		int 	buttonDown;
+		int		buttonLeft;
+		int 	buttonRight;
+		int		buttonStart;
+		int 	buttonStop;
+	
+	//metody
+				RemoteButton(int);
+		int 	readIRSignal(IRrecv); //oddaje wartość odczytaną z odbiornika IR
 };
 
 
-RemoteButton::RemoteButton(int h1)
-{
-	hexCode1 = h1;
-	hexCode2 = hexCode1 + 0x10000;
-}
+	RemoteButton::RemoteButton(int h1=0x0)
+	{
+		hexCode1 = h1;
+		hexCode2 = hexCode1 + 0x10000;
+		int pin = 10; 					//!! pin odbiornika IR zawsze 10! ZMIENIĆ JEŚLI INNY
+		
+		buttonUp = 0x3058;
+		buttonDown = 0x3059;
+		buttonLeft = 0x305A;
+		buttonRight = 0x305B;
+		buttonStart = 0x302C;
+		buttonStop = 0x3031;
+	}
 
-
+	int RemoteButton::readIRSignal(IRrecv irrecv)
+	{
+		decode_results results;
+		if (irrecv.decode(&results)) 
+		{
+			delay(250);
+			irrecv.resume();
+			Serial.println(results.value, HEX);
+			return results.value;
+		}
+		else return NULL;
+	}
 
 
 /*
@@ -129,7 +163,7 @@ FUNKCJE
 
 //przewijanie napisu po ekranie gdy ilość znaków większa od 16 
  void lcd16RollString (LiquidCrystal lcd, String napis, int rzad)
-  {
+{
     if(napis.length()>16){
     for(int i =0;i<=(napis.length()-16);i++)
     {
@@ -145,8 +179,10 @@ FUNKCJE
     for(int i=0;i<napis.length();i++){
     lcd.print(napis[i]);}
     }
-  }
+}
   
+  
+//wyświetlanie wartości liczbowej, 4 ostatnie znaki na wyświetlaczu lcd
 void displayTemp(float value, LiquidCrystal lcd, int row)
 {
 	if (value < 10) //liczby jednocyfrowe
--- v0.ino
+++ v0.ino
@@ -2,11 +2,15 @@
 #include "fun.h"
 
 
-const int LED = 9;
+const int LED = 11;
+/*
+nieużywane przyciski
 Button buttonPlus(10);
 Button buttonMinus(11);
-Button buttonAccept(12);
+Button buttonAccept(12);*/
 Temperature t1(20,15);
+RemoteButton pilot;
+IRrecv irrecv(10);
 bool ON = false;
 
 LiquidCrystal lcdDisplay(2,3,4,5,6,7);
@@ -15,62 +19,69 @@ LiquidCrystal lcdDisplay(2,3,4,5,6,7);
 //funkcje
 void lcd16RollString (LiquidCrystal lcd, String napis, int rzad);
 void displayTemp(float value, LiquidCrystal lcd, int row); //wyświetl wartość value temperatury w wierszu row wyświetlacza lcd
-void buttonPlus_OnClick();
-void buttonMinus_OnClick();
-bool buttonAccept_OnClick();
 
 
 void setup() {
 	//ustawienie pinów
 	pinMode(LED,OUTPUT);
-  digitalWrite(LED,LOW);
+	digitalWrite(LED,LOW);
+	irrecv.enableIRIn();
+	Serial.begin(9600);
 	
 	//ustawienie ekranu
-  lcdDisplay.begin(16,2);
-  lcdDisplay.setCursor(0,0);
-  lcdDisplay.print("Temperatura: ");
-  displayTemp(t1.getTempValue(),lcdDisplay,0);
-  
-  lcdDisplay.setCursor(0,1);
-  lcdDisplay.print("Ustaw temp.");
-  displayTemp(t1.desired,lcdDisplay,1);
+	  lcdDisplay.begin(16,2);
+	  lcdDisplay.setCursor(0,0);
+	  lcdDisplay.print("Temperatura: ");
+	  displayTemp(t1.getTempValue(),lcdDisplay,0);
+	  
+	  lcdDisplay.setCursor(0,1);
+	  lcdDisplay.print("Ustaw temp.");
+	  displayTemp(t1.desired,lcdDisplay,1);
 }
 
 void loop() 
 {
-	buttonPlus_OnClick();
-	buttonMinus_OnClick();
 	displayTemp(t1.desired, lcdDisplay, 1);
 	displayTemp(t1.getTempValue(), lcdDisplay, 0);
-	while( buttonAccept_OnClick())
-	{
-		t1.tempSimulation(t1.isHeatingReq());
-		displayTemp(t1.desired, lcdDisplay, 1);
-		displayTemp(t1.getTempValue(), lcdDisplay, 0);
-	}		
+	test2();
+	//t1.tempSimulation();
 }
 
-
-void buttonPlus_OnClick()
+/*
+void test()
 {
-  if(buttonPlus.isPushed()) 
-  {
-    t1.increaseDesired(0.5);
-    delay(150);
-  }
-}
-
-void buttonMinus_OnClick()
-{
-	if(buttonMinus.isPushed())
+	switch(pilot.readIRSignal(irrecv))
 	{
-		t1.decreaseDesired(0.5);
-		delay(150);
+	//gora
+		case pilot.buttonUp:
+			t1.increaseDesired(0.5); break;
+		//case 0x13058: t1.increaseDesired(0.5); break;
+		case pilot.buttonDown:
+			t1.decreaseDesired(0.5); break;
+		//case 0x13059: t1.decreaseDesired(0.5); break;
+		case (pilot.buttonStart):
+			while(pilot.readIRSignal(irrecv) != pilot.buttonStop)
+			{
+				t1.tempSimulation();
+			}
+		default: break;
 	}
-}
+	
+}*/
 
-bool buttonAccept_OnClick()
+void test2()
 {
-	if(buttonAccept.isPushed()) return ON = !ON;
-	else return ON;
+	if (pilot.readIRSignal(irrecv) == pilot.buttonUp) t1.increaseDesired(0.5);
+	if (pilot.readIRSignal(irrecv) == pilot.buttonDown) t1.decreaseDesired(0.5);
+	else if (pilot.readIRSignal(irrecv) == pilot.buttonStart)
+	{
+		bool stopSignal=true;
+		while(stopSignal == true)
+			{
+				Serial.println("hejka");
+				t1.tempSimulation();
+				displayTemp(t1.getTempValue(), lcdDisplay, 0);
+				if(pilot.readIRSignal(irrecv) == pilot.buttonStop) stopSignal = false;
+			}
+	}
 }
