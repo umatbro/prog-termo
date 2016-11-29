87a2b2edb9636357aa596e57d9d910a72fb00e3f
umatbro <umatbroo@gmail.com>
2016-11-29 03:43:53 +0100

    Add files via upload

--- button-setting.ino
+++ button-setting.ino
@@ -9,7 +9,7 @@ LiquidCrystal lcdDisplay(2,3,4,5,6,7);
 Button buttonPlus(8,PULLUP);
 Button buttonMinus(9,PULLUP);
 Button buttonAccept(12);
-Button *flag;
+Button *recentlyPressed; //flaga recently pressed
 Temperature tDesired(20);
 
 //funkcje
@@ -38,23 +38,23 @@ void loop()
 	if(buttonPlus.uniquePress())
 	{
 		displayTemp(tDesired.set( tDesired.value() + 0.5 ), lcdDisplay, 1);
-		flag = &buttonPlus;
+		recentlyPressed = &buttonPlus;
 	}
 	if (buttonMinus.uniquePress())
 	{
 		displayTemp(tDesired.set( tDesired.value() - 0.5 ), lcdDisplay, 1);
-		flag = &buttonMinus;
+		recentlyPressed = &buttonMinus;
 	}
-	unsigned long czas = flag -> timePressed();
+	unsigned long czas = recentlyPressed -> timePressed();
 	if(czas>1000 && czas%200 < 10)
 	{
-		if(flag == &buttonPlus) tDesired.set( tDesired.value() + 0.5 );
-		if(flag == &buttonMinus) tDesired.set( tDesired.value() - 0.5 );
+		if(recentlyPressed == &buttonPlus) tDesired.set( tDesired.value() + 0.5 );
+		if(recentlyPressed == &buttonMinus) tDesired.set( tDesired.value() - 0.5 );
 		displayTemp(tDesired.value(), lcdDisplay, 1);
 	}
 	
 
-	Serial.println(flag -> timePressed());
+	Serial.println(recentlyPressed -> timePressed());
 }
 
 
