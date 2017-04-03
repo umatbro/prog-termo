d9fed1c3c99a0691b0e26343d730e55e40513471
umatbro <umatbroo@gmail.com>
2017-04-03 14:44:46 +0200

    tekst i prezentacja

---
+++ docs/Załącznik 2/Button.h
@@ -0,0 +1,41 @@
+/*
+||
+|| @file Button.h
+|| @version 1.6
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Provide an easy way of making buttons
+|| #
+||
+|| @license
+|| | Copyright (c) 2009 Alexander Brevig. All rights reserved.
+|| | This code is subject to AlphaLicence.txt
+|| | alphabeta.alexanderbrevig.com/AlphaLicense.txt
+|| #
+||
+*/
+#include "Arduino.h"
+#define PULLUP HIGH
+#define PULLDOWN LOW
+
+#define CURRENT 0
+#define PREVIOUS 1
+#define CHANGED 2
+
+class Button{
+  public:
+    Button(uint8_t buttonPin, uint8_t buttonMode=PULLDOWN);
+    void pullup();
+    void pulldown();
+    bool isPressed();
+    bool wasPressed();
+    bool stateChanged();
+	bool uniquePress();
+	unsigned long timePressed();
+  private: 
+    uint8_t pin;
+    uint8_t mode;
+    uint8_t state;
+};
---
+++ docs/Załącznik 3/proj.h
@@ -0,0 +1,73 @@
+﻿#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+#include "Arduino.h"
+#include "Button/Button.h"
+#include "max6675.h"
+
+class Button;
+
+/*
+TEMPERATURA
+*/
+class Temperature
+{
+	private:
+		float val; //obecna wartość temperatury
+	
+	public:
+				Temperature(float=0); //konstruktor
+		float 	getTempValue(MAX6675);
+		float	value();
+		float 	set(float); //ustawia podaną temperaturę i zwraca jej wartość
+};
+
+
+/*
+	TIMER
+*/
+class Timer
+{
+	private:
+		unsigned long 	millisecNow;
+		unsigned long 	millisecStart;
+	public: 
+						Timer();
+		long int		threshold;
+		boolean 		stepTimer(unsigned long); //zwraca true cyklicznie co określoną ilość milisekund, pozwala na wykonanie jakiejś akcji co pewien czas
+};
+
+/*
+	POWER SUPPLY - MANSON 2405
+*/
+class Manson2405
+{
+	public:
+		String sendCommand(SoftwareSerial, String); //wysyła komendę do zasilacza
+		String startSession(SoftwareSerial, int); // wyłącza przedni panel
+		String endSession(SoftwareSerial, int); // włącza przedni panel i kończy sesję
+};
+
+/*	
+	Regulator PID
+*/
+	
+class RegPID 
+{
+	private:
+		float k;
+		float k_i;
+		float k_d;
+		
+	public:
+		void setK(float);
+		void setKI(float);
+		void setKD(float);
+		RegPID(float =100,float=0.002,float =20);
+		float regulator(float , float );
+};
+
+
+
+//INNE
+extern void lcdPrint(LiquidCrystal lcd, String napis, int rzad);
+extern void displayTemp(float value, LiquidCrystal lcd, int row);
